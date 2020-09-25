1b60d3761ac69c5b6fb9a64c0a27e6d737c0a63c
tonmoycsedu <tonmoycsedu@gmail.com>
2020-09-25 04:26:25 -0400

    added sections

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 $("a").click(function() {
 	id = $(this).attr('id')
 	div_id = '#'+id+'_div'
-    $('html, body').animate({
-        scrollTop: $(div_id).offset().top
-    }, 2000);
+	$(".section").hide()
+    $(div_id).show()
 });
